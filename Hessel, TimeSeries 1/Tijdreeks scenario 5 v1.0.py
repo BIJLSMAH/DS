f0be3de2a1b40cf5ab60079f699916dd7e31564c
bijlsmah <h.bijlsma@dictu.nl>
2020-04-21 13:44:33 +0200

    Oplevering definitief script

--- Hessel, TimeSeries 1/Tijdreeks scenario 5 v1.0.py
+++ Hessel, TimeSeries 1/Tijdreeks scenario 5 v1.0.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
 from pmdarima import auto_arima
+from datetime import datetime
+
+def monthlist(dates):
+    start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
+    total_months = lambda dt: dt.month + 12 * dt.year
+    mlist = []
+    for tot_m in range(total_months(start)-1, total_months(end)):
+        y, m = divmod(tot_m, 12)
+        mlist.append(datetime(y, m+1 ,1))
+    return mlist
 
 os.chdir(r'C:\Users\Administrator\Documents\GitHub\DS\Hessel, TimeSeries 1')
 data = pd.read_csv(r'data\MAAND OPEN PRD 2014-2019.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
@@ -46,6 +56,10 @@ plt.xlabel('Jaren')
 plt.ylabel('Residual')
 plt.show()
 
+plt.rcParams.update({'figure.figsize': (10, 10)})
+result.plot().suptitle('Multiplicatieve Decompositie', fontsize=22)
+plt.show()
+
 yshow = y[:len(y-11)]
 y, ytest = y[:(len(y)-12)], y[(len(y)-12):len(y)]
 # The term bfill means that we use the value before filling in missing values
@@ -69,13 +83,21 @@ stepwise_model = auto_arima(y, start_p=1, start_q=1,
                            stepwise=True)
 
 print(stepwise_model.aic())
-predm12 = pd.Series(stepwise_model.predict(n_periods=len(ytest)), index=ytest.index)
+# Doorloop de maanden en verzamel maandgegevens over de totalen
+dates=["2019-01-01", "2020-12-01"]
+mndLijst = monthlist(dates)
+
+predm12 = pd.Series(stepwise_model.predict(n_periods=len(ytest)*2), index=mndLijst)
+lower_series = ytest - 0.15 * ytest
+upper_series = ytest + 0.15 * ytest
 plt.figure(figsize=(8, 3), dpi=100)
 plt.plot(yshow, label='training')
 plt.plot(ytest, label='actual')
 plt.plot(predm12, label='forecast')
 plt.xlabel('jaren')
 plt.ylabel('incidenten')
+plt.fill_between(lower_series.index, lower_series, upper_series,
+                 color='k', alpha=.15)
 plt.tight_layout(pad=3.0)
 plt.title('Forecast vs Actuals')
 plt.legend(loc='upper left', fontsize=8)

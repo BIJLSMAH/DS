104b41342c6d92fddf617dc766445a9eeb37610b
HBijlsma <57830221+h-bijlsma@users.noreply.github.com>
2020-05-14 15:00:21 +0200

    Wijziging TM dd 20200514

--- Hessel, TextMining 1/TM KMeans Supervised v0.3.py
+++ Hessel, TextMining 1/TM KMeans Supervised v0.3.py
@@ -42,7 +42,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # load the dataset
-hoofdmap = r'C:\Users\Administrator\Documents\GitHub\DS\Hessel, TextMining 1'
+
+mapGebruikers = "C:\\Users\\"
+mapGebruiker = os.getlogin()
+mapTM = r'\Documents\Github\DS\Hessel, TextMining 1'
+hoofdmap = "%s%s%s" %(mapGebruikers, mapGebruiker, mapTM)
+
+#hoofdmap = r"C:\Users\" + os.getlogin() + r"\Documents\GitHub\DS\Hessel, TextMining 1"
 os.chdir(hoofdmap)
 # data = pd.read_excel(r'data\Incidenten_SD_2018_2019_totaal.xlsm', header=0, parse_dates=False, squeeze=True)
 # data.to_csv(r'data\Incidenten_SD_2018_2019_totaal-v3.csv')
@@ -177,7 +183,7 @@ df['Object ID'].fillna("Onbekend", inplace = True)
 chosen_idx = np.random.choice(len(df), replace=False, size=int(len(df)/2))
 df = df.iloc[chosen_idx]
 
-df['Korte omschrijving Details'][1]
+df['Korte omschrijving Details']
 
 # Vervangen sturingskarakters.
 df['Korte omschrijving Details'] = df['Korte omschrijving Details'].str.replace("\r"," ")
@@ -661,3 +667,166 @@ def document_features(document):
 
 for i in range(0, len(documentslst)):
     print(document_features(documentslst[i]))
+#%% MODEL TRAINING
+"""
+# 5. Multinomial logistic 
+
+"""
+# import pickle
+# import numpy as np
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.linear_model import LogisticRegression
+# from pprint import pprint
+# from sklearn.model_selection import RandomizedSearchCV
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+# from sklearn.model_selection import ShuffleSplit
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pandas as pd
+
+# Haal gegevens op
+# os.chdir(hoofdmap)
+# Dataframe
+# path_df = "data/df.pickle"
+# with open(path_df, 'rb') as data:
+#     df = pickle.load(data)
+# Dataframe unique top ObjectID
+# path_df2 = "data/df2.pickle"
+# with open(path_df, 'rb') as data:
+#    df = pickle.load(data)
+# features_train
+# path_features_train = "data/features_train.pickle"
+# with open(path_features_train, 'rb') as data:
+#   features_train = pickle.load(data)
+# labels_train
+# path_labels_train = "data/labels_train.pickle"
+# with open(path_labels_train, 'rb') as data:
+#    labels_train = pickle.load(data)
+# features_test
+# path_features_test = "data/features_test.pickle"
+# with open(path_features_test, 'rb') as data:
+#     features_test = pickle.load(data)
+# labels_test
+# path_labels_test = "data/labels_test.pickle"
+# with open(path_labels_test, 'rb') as data:
+#     labels_test = pickle.load(data)
+# print(features_train.shape)
+# print(features_test.shape)
+
+# lr_0 = LogisticRegression(random_state = 8)
+
+# print('Parameters currently in use:\n')
+# pprint(lr_0.get_params())
+
+# C
+# C = [float(x) for x in np.linspace(start = 0.1, stop = 1, num = 10)]
+
+# multi_class
+# multi_class = ['multinomial']
+
+# solver
+# solver = ['newton-cg', 'sag', 'saga', 'lbfgs']
+ 
+# class_weight
+# class_weight = ['balanced', None]
+
+# penalty
+# penalty = ['l2']
+
+# Create the random grid
+# random_grid = {'C': C,
+#                'multi_class': multi_class,
+#                'solver': solver,
+#                'class_weight': class_weight,
+#                'penalty': penalty}
+
+# pprint(random_grid)
+
+# First create the base model to tune
+# lrc = LogisticRegression(random_state=8)
+
+# Definition of the random search
+# random_search = RandomizedSearchCV(estimator=lrc,
+#                                    param_distributions=random_grid,
+#                                    n_iter=50,
+#                                    scoring='accuracy',
+#                                    cv=3, 
+#                                    verbose=1, 
+#                                    random_state=8)
+
+# Fit the random search model
+# random_search.fit(features_train, labels_train)
+
+# print("The best hyperparameters from Random Search are:")
+# print(random_search.best_params_)
+# print("")
+# print("The mean accuracy of a model with these hyperparameters is:")
+# print(random_search.best_score_)
+
+# Create the parameter grid based on the results of random search 
+# C = [float(x) for x in np.linspace(start = 0.6, stop = 1, num = 10)]
+# multi_class = ['multinomial']
+# solver = ['sag']
+# class_weight = ['balanced']
+# penalty = ['l2']
+
+param_grid = {'C': C,
+               'multi_class': multi_class,
+               'solver': solver,
+               'class_weight': class_weight,
+               'penalty': penalty}
+
+# Create a base model
+lrc = LogisticRegression(random_state=8)
+
+# Manually create the splits in CV in order to be able to fix a random_state (GridSearchCV doesn't have that argument)
+cv_sets = ShuffleSplit(n_splits = 3, test_size = .33, random_state = 8)
+
+# Instantiate the grid search model
+grid_search = GridSearchCV(estimator=lrc, 
+                           param_grid=param_grid,
+                           scoring='accuracy',
+                           cv=cv_sets,
+                           verbose=1)
+
+# Fit the grid search to the data
+grid_search.fit(features_train, labels_train)
+
+print("The best hyperparameters from Grid Search are:")
+print(grid_search.best_params_)
+print("")
+print("The mean accuracy of a model with these hyperparameters is:")
+print(grid_search.best_score_)
+
+best_lrc = grid_search.best_estimator_
+best_lrc
+
+best_lrc.fit(features_train, labels_train)
+
+lrc_pred = best_lrc.predict(features_test)
+
+# Training accuracy
+print("The training accuracy is: ")
+print(accuracy_score(labels_train, best_lrc.predict(features_train)))
+
+# Test accuracy
+print("The test accuracy is: ")
+print(accuracy_score(labels_test, lrc_pred))
+
+# Classification report
+print("Classification report")
+print(classification_report(labels_test,lrc_pred))
+
+# aux_df = df[['Category', 'Category_Code']].drop_duplicates().sort_values('Category_Code')
+# conf_matrix = confusion_matrix(labels_test, lrc_pred)
+# plt.figure(figsize=(12.8,6))
+# sns.heatmap(conf_matrix, 
+#             annot=True,
+#             xticklabels=aux_df['Category'].values, 
+#             yticklabels=aux_df['Category'].values,
+#             cmap="Blues")
+# plt.ylabel('Predicted')
+# plt.xlabel('Actual')
+# plt.title('Confusion matrix')
+# plt.show()
---
+++ Hessel, TextMining 1/TM XGBoost v0.1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 14:46:33 2020
+
+@author: Laptop
+"""
+
+
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
